1cadcd42395e838e989442b3fd74be9beec10b52
DoOrQuit <kosankos@gmail.com>
2022-07-31 12:17:29 +0300

    construction revised

--- service_me_main.py
+++ service_me_main.py
@@ -1,8 +1,3 @@
-from io import BufferedRandom
-from pyexpat import model
-from sys import _enablelegacywindowsfsencoding
-
-
 class Service_me():
     def __init__(self, brand, model, engine, transmition, mileage):
         self.brand = brand
