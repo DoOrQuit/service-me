3e7278bedbcb7b642fb4d65ab0717bea4f2a6938
DoOrQuit <kosankos@gmail.com>
2022-08-01 18:09:10 +0300

    client_int method added

--- service_me_main.py
+++ service_me_main.py
@@ -1,7 +1,37 @@
+import psycopg2
+from Security.db_connection import db_connection
+
+
+def db_get_connection(): 
+    """ Connect to the PostgreSQL database server """
+    db_connection()
+    
+
 class Service_me():
-    def __init__(self, brand, model, engine, transmition, mileage):
-        self.brand = brand
-        self.model = model
-        self.engine = engine
-        self.transmition = transmition
+    
+    def __init__(self, first_name, last_name, car_number_plate, mileage=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.car_number_plate = car_number_plate
         self.mileage = mileage
+
+    
+    def client_init(self):
+        
+        """Client Identification"""
+        
+        with conn.cursor() as curs:
+            curs.execute("SELECT * FROM clients WHERE last_name = '%s' and car_number_plate = '%s' ", (self.last_name.capitalize(), self.car_number_plate.upper()))
+        return curs.fetchall()
+    
+if __name__ == '__main__':
+    customer1 = Service_me(Valentyn, Shevchenko, AA5533AA)
+
+
+    
+
+
+        
+        
+
+        
