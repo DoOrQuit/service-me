3b7861ee1b6db61e8cb1dce941c1bded50a67ff1
DoOrQuit <kosankos@gmail.com>
2022-08-02 19:09:43 +0300

    Maintenance_perform() added to record the current mileage and service date to SQL DB

--- service_me_main.py
+++ service_me_main.py
@@ -1,40 +1,46 @@
 import psycopg2
 from datetime import date
+import time
 
 
 class Service_me():
     
-    def __init__(self, first_name, last_name, car_number_plate, mileage=0):
+    def __init__(self, first_name, last_name, car_number_plate, mileage = 0):
         self.first_name = first_name
         self.last_name = last_name
         self.car_number_plate = car_number_plate
         self.mileage = mileage
+        self.today = date.today()
     
     
     def client_init(self):
         
         """Client Identification indicates client ID, Name, Phone_number, Car brand and Car Number PLate"""
         try :
-            with psycopg2.connect(database = 'car_service_db', user = 'postgres', password = 'datapass') as conn:
-                with conn.cursor() as curs:
-                    curs.execute("SELECT * FROM clients WHERE last_name = %s and car_number_plate = %s ", (self.last_name.capitalize(), self.car_number_plate.upper()))
-                    client_info = curs.fetchall()
-                    print(client_info[0:6])
+            conn = psycopg2.connect(database = 'car_service_db', user = 'postgres', password = 'datapass')
+            curs = conn.cursor()
+            curs.execute("""
+                        SELECT * 
+                        FROM clients
+                        WHERE last_name = %s and car_number_plate = %s """, (self.last_name.capitalize(), self.car_number_plate.upper()))
+            client_info = curs.fetchall()
+            print(client_info[0:6])
         finally:
             curs.close()
             conn.close()
     
-    def service_plan(self):
+    def maintenance_plan(self):
 
         """List of works planned depends on mileage of a client"""
 
         try:
-            with psycopg2.connect(database = 'car_service_db', user = 'postgres', password = 'datapass') as conn:
-                with conn.cursor() as curs:
-                    curs.execute("SELECT maintenance_id, maintenance_type, maintenance_1 FROM kia")
+            conn = psycopg2.connect(database = 'car_service_db', user = 'postgres', password = 'datapass')
+            curs = conn.cursor()
+            curs.execute("SELECT maintenance_id, maintenance_type, maintenance_1 FROM kia")
             work_list = curs.fetchall()
             
             if self.mileage == 0 :
+                time.sleep(2)
                 print(f"Make sure You've entered a correct Milieage!")
             elif 17000 >= self.mileage > 0 :
                 curs.execute("""
@@ -42,7 +48,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_1 = 'check' or maintenance_1 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #1')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
 
@@ -52,7 +60,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_2 = 'check' or maintenance_2 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #2')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
 
@@ -62,7 +72,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_3 = 'check' or maintenance_3 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #3')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
 
@@ -72,7 +84,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_4 = 'check' or maintenance_4 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #4')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
             
@@ -82,7 +96,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_5 = 'check' or maintenance_5 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #5')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
 
@@ -92,7 +108,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_6 = 'check' or maintenance_6 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #6')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
                 
@@ -102,7 +120,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_7 = 'check' or maintenance_7 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #7')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
             
@@ -112,7 +132,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_8 = 'check' or maintenance_8 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #8')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
             
@@ -122,7 +144,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_9 = 'check' or maintenance_9 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #9')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
             
@@ -132,7 +156,9 @@ class Service_me():
                             FROM kia 
                             WHERE maintenance_10 = 'check' or maintenance_10 = 'replacement' """)
                 work_list = curs.fetchall()
+                time.sleep(2)
                 print('Here are all works we planning to carry out within Maintenance #10')
+                time.sleep(2)
                 for raw in work_list:
                     print(raw)
         
@@ -140,20 +166,52 @@ class Service_me():
             curs.close()
             conn.close()
 
+    def maintenance_perform(self):
+
+        """Performs maintenance with apropriate records in DataBase"""
         
-        
-        
-    
+        while True:
+
+            request = str(input('Are You agree with terms of use and ready to begin the maintenance? [y/n]')).lower()
+                              
+            if request == 'n':
+                time.sleep(2)
+                print('Thank You visiting our Car Service. If You need any additional support please contact our manager.')
+                break
                 
-    
+            elif request == 'y':
+                time.sleep(2)
+                print('Thank You for choosing our Car Service. You may have a cup of tea/coffee in our lobby')
+                time.sleep(2)
 
+                
+                try:
+                    conn = psycopg2.connect(database = 'car_service_db', user = 'postgres', password = 'datapass')
+                    curs = conn.cursor()
+                    curs.execute("""
+                                    UPDATE clients 
+                                    SET last_mileage = '%s', service_date = %s 
+                                    WHERE car_number_plate = %s""", (self.mileage, self.today, self.car_number_plate))
+                    
 
+                finally:
+                    conn.commit()
+                    curs.close()
+                    conn.close()
+                
+                time.sleep(1)
+                print('Maintenance is done. Data recorded in DataBase successfully!')
+                break
+            
+            else:
+                print('Please, enter a correct input. Agree[y] or Refuse[n]')
+                continue
 
 
 
 if __name__ == '__main__':
-    client1 = Service_me('Valentyn', 'Shevchenko', 'AA5533AA', 5000)
-    client1.service_plan()
+    client1 = Service_me('Valentyn', 'Shevchenko', 'AA5533AA', 28000)
+    client1.maintenance_perform()
 
 
 
